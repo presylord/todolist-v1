1973acaca57806dabcd554fc2353678033137867
Presy Lord Kidlat <kidlat123@gmail.com>
2021-11-08 20:30:12 +0800

    added ejs forloop

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
-
+var entryArray = [];
+var newEntry = "";
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.write("It's a weekend");
-  } else {
-    res.write("It's a weekday");
-  }
-  res.send();
+  var options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  var completeDate = today.toLocaleDateString("en-US", options);
+  res.render("list", {
+    currentDay: completeDate,
+    entryArray: entryArray,
+  });
 });
 
-// app.post();
+app.post("/", function (req, res) {
+  var newEntry = req.body.newItem;
+  entryArray.push(newEntry);
+  res.redirect("/");
+});
+// app.post("/reset", function (req, res) {
+//   entryArray = [];
+//   res.redirect("/");
+// });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
