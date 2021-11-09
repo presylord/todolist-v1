e20ac02612f4c24fe5ce1131ab4dce2e6ce50f4f
Presy Lord Kidlat <kidlat123@gmail.com>
2021-11-10 01:04:18 +0800

    styled todo list

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-var items = [];
+var items = ["go to gas station", "pay bills", "buy grocery"];
 var newItem = "";
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -12,7 +13,6 @@ app.get("/", function (req, res) {
     weekday: "long",
     month: "long",
     day: "numeric",
-    year: "numeric",
   };
   var completeDate = today.toLocaleDateString("en-US", options);
   res.render("list", {
