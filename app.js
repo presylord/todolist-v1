1a81fca5577add7f48c2af34bc42c4beb7b27690
presylord <kidlat123@gmail.com>
2021-11-10 13:21:24 +0800

    added new tab

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 var items = ["go to gas station", "pay bills", "buy grocery"];
 var newItem = "";
+
+var workItems = ["create weekly report", "submit daily updates"];
+var newWorkItem = "";
+
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,18 +20,30 @@ app.get("/", function (req, res) {
   };
   var completeDate = today.toLocaleDateString("en-US", options);
   res.render("list", {
-    currentDay: completeDate,
+    listTitle: completeDate,
     items: items,
   });
 });
 
-app.post("/", function (req, res) {
-  var newItem = req.body.newItem;
-  items.push(newItem);
-  res.redirect("/");
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", items: workItems });
 });
+
+app.post("/", function (req, res) {
+  if (req.body.list === "Work List") {
+    var newWorkItem = req.body.newItem;
+    workItems.push(newWorkItem);
+    res.redirect("/work");
+  } else {
+    var newItem = req.body.newItem;
+    items.push(newItem);
+    res.redirect("/");
+  }
+});
+
 app.post("/reset", function (req, res) {
   items = [];
+  workItems = [];
   res.redirect("/");
 });
 
